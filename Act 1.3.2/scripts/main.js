cb4cd0be8c6d5d81f04d2528e31b4cbe7b34ddb1
bas <bas.cares@duocuc.cl>
2025-08-27 10:23:03 -0400

    algo

---
+++ Act 1.3.2/scripts/main.js
@@ -0,0 +1,10 @@
+
+let message = "Welcome to League of Legends";
+
+console.log(message);
+
+document.writeln("<h1>"+message+"</h1>");
+
+const img = document.createElement('img');
+img.src = '3vrnc4w9z1df1.jpeg';
+document.body.appendChild(img);
